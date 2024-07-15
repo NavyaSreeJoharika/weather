cfdf13c644458df98a7e5c9aa1c48ebcf618a283
NavyaSreeJohairka <navyasreejohairka@gmail.com>
2024-07-15 11:26:15 +0530

    commit

--- weather-cli/main.go
+++ weather-cli/main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
+
+type Location struct {
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
+type Condition struct {
+	Text string `json:"text"`
+}
+
+type Current struct {
+	TempC     float64   `json:"temp_c"`
+	Condition Condition `json:"condition"`
+}
+
+type HourlyForecast struct {
+	TimeEpoch    int64     `json:"time_epoch"`
+	TempC        float64   `json:"temp_c"`
+	Condition    Condition `json:"condition"`
+	ChanceOfRain float64   `json:"chance_of_rain"`
+}
+
+type DailyForecast struct {
+	Hour []HourlyForecast `json:"hour"`
+}
+
+type Forecast struct {
+	Forecastday []DailyForecast `json:"forecastday"`
+}
+
+type Weather struct {
+	Location Location `json:"location"`
+	Current  Current  `json:"current"`
+	Forecast Forecast `json:"forecast"`
+}
 
 func main() {
 	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=ec180872243c4f57a4f153631230105&q=Iasi&days=1&aqi=no&alerts=no")
@@ -22,6 +59,34 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	//fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf(
+		"%s, %s: %.0fC, %s\n",
+		location.Name,
+		location.Country,
+		current.TempC,
+		current.Condition.Text,
+	)
+
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		fmt.Printf(
+			"%s - %.0fC: %.0f, %s\n",
+			date.Format("15:04"),
+			hour.TempC,
+			hour.ChanceOfRain,
+			hour.Condition.Text,
+		)
+	}
+	fmt.Println(time.Now().Date())
 
 }
